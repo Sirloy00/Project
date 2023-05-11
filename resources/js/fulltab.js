481c6744c700b872916e4322eadf2bfb4f2b370f
Carlo C <carlo.cruz0215@gmail.com>
2023-05-12 04:10:35 +0800

    Portfolio

--- resources/js/fulltab.js
+++ resources/js/fulltab.js
@@ -11,6 +11,6 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
-
